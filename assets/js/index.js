e6627c835a5d00acf7d1989d3ffedc68b060153b
4RomanR <4rprogramming@gmail.com>
2023-11-09 14:06:39 -0500

    Porfolio organizado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,32 @@
 const btnOpen = document.querySelector(".btn-open");
 const btnClose = document.querySelector(".btn-close");
-const menu =  document.querySelector(".nav-open");
-
+const menu =  document.querySelector(".nav-bar-list");
+const listLink = document.querySelector('.list-link')
 btnOpen.addEventListener('click', function (){
     menu.classList.toggle('nav-open')
     console.log('apertura')
 })
-btnOpen.addEventListener('click', function (){
+btnClose.addEventListener('click', function (){
+    menu.classList.toggle('nav-open')
+    console.log('cierre')
+})
+listLink.addEventListener('click', function (){
     menu.classList.remove('nav-open')
+    console.log('cierre')
+})
+
+
+
+//----------------
+const html = document.documentElement
+console.log(html.scrollHeight)
+
+
+const progressHtml = document.querySelector('.progress')
+window.addEventListener('scroll', function(e){
+    const pageHeight = html.scrollHeight - html.clientHeight
+    const currentScroll = html.scrollTop
+    const progress = currentScroll / pageHeight
+    console.log(progressHtml.style.width = Math.round(progress * 100)+'%')
+
 })
